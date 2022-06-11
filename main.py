ebd944806331982d483d1022d1674f475afef6ae
Malkov Dima <malkovich.23@hotmail.com>
2022-06-12 00:07:50 +0500

    1 2 задачи сделаны

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-cook_book = {}
-h = ['Омлет', 'Запеченный картофель']
-
-
 def print_cook(menu):
     dish = menu.readline().strip()
     n = int(menu.readline().strip())
     ingredient_list = []
     ingredient_dict = {}
-    i = 0
     for i in range(n):
         str_ind = menu.readline().strip()
         ingredient_dict['ingredient_name'] = str_ind[0:str_ind.find(' | ')]
-        str_ind = str_ind[str_ind.find(' | ')+3:]
+        str_ind = str_ind[str_ind.find(' | ') + 3:]
         ingredient_dict['quantity'] = int(str_ind[0:str_ind.find(' | ')])
         str_ind = str_ind[str_ind.find(' | ') + 3:]
         ingredient_dict['measure'] = str_ind
@@ -21,7 +16,7 @@ def print_cook(menu):
     menu.readline()
 
 
-def empty_str(file_name):
+def empty_str():
     with open('cook.txt') as file:
         lst = [i for i in file.readlines() if '\n' == i]
     return len(lst)
@@ -29,18 +24,17 @@ def empty_str(file_name):
 
 def all_menu():
     with open('cook.txt') as file_cook:
-        for g in range(empty_str(file_cook)+1):
+        for g in range(empty_str() + 1):
             print_cook(file_cook)
     return cook_book
 
 
-def print_menu(list_menu):
-    with open('cook.txt') as file_cook:
-        list_dish = []
-        print("Меню нашего заведения:")
-        for key in list_menu:
-            list_dish.append(key)
-        print(list_dish)
+def menu_in_list(list_menu):
+    list_dish = []
+    print("Меню нашего заведения:")
+    for key in list_menu:
+        list_dish.append(key)
+    return list_dish
 
 
 def get_shop_list_by_dishes(dishes: list, person_count: int):
@@ -53,7 +47,7 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
                 all_product[name]['quantity'] += n['quantity']
             else:
                 all_product[name] = n
-    if person_count >1:
+    if person_count > 1:
         for k in all_product.values():
             k['quantity'] *= person_count
     for key, value in all_product.items():
@@ -61,10 +55,16 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
     return all_product
 
 
-all_menu()
+cook_book = {}
+list_dishes = ['Омлет', 'Запеченный картофель']
+person = 3
+all_menu()  # функция формирование словоря cook_book
 print(cook_book)
-print('+++++++++')
-print_menu(cook_book)
-print('+++++++++')
-get_shop_list_by_dishes(h, 9)
-
+print('-------------------')
+print()
+print(menu_in_list(cook_book))  # Список рецептов файла
+print('-------------------')
+print()
+get_shop_list_by_dishes(list_dishes, person)  # функция подсчета ингредиентов на входе список блюд и количество персон
+print('-------------------')
+print()
